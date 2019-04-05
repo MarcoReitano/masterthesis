3eff9fd473af68ee26fa06b0642e98314077acb7
Marco Reitano <marco.reitano@th-koeln.de>
2019-04-05 16:14:52 +0200

    Third commit, now with all files added

--- Frontend/0_Website/html/artist-list.js
+++ Frontend/0_Website/html/artist-list.js
@@ -1,5 +1,5 @@
-import {html, PolymerElement} from './node_modules/@polymer/polymer/polymer-element.js';
-import './node_modules/@polymer/polymer/lib/elements/dom-repeat.js'
+import { html, PolymerElement } from './node_modules/@polymer/polymer/polymer-element.js';
+import './node_modules/@polymer/polymer/lib/elements/dom-repeat.js';
 import './node_modules/@polymer/iron-ajax/iron-ajax.js';
 /**
  * `artist-list`
@@ -9,8 +9,9 @@ import './node_modules/@polymer/iron-ajax/iron-ajax.js';
  * @polymer
  * @demo demo/index.html
  */
+
 class ArtistList extends PolymerElement {
-  constructor(){
+  constructor() {
     super();
     this["JSON.stringify"] = JSON.stringify;
   }
@@ -35,6 +36,7 @@ class ArtistList extends PolymerElement {
       </template>
     `;
   }
+
   static get properties() {
     return {
       artists: {
@@ -42,5 +44,7 @@ class ArtistList extends PolymerElement {
       }
     };
   }
+
 }
+
 window.customElements.define('artist-list', ArtistList);
---
+++ Frontend/artist/artist-element.js
@@ -0,0 +1,71 @@
+import {html, PolymerElement} from './node_modules/@polymer/polymer/polymer-element.js';
+
+/**
+ * `artist-element`
+ * 
+ *
+ * @customElement
+ * @polymer
+ * @demo demo/index.html
+ */
+class ArtistElement extends PolymerElement {
+  static get template() {
+    return html`
+      <style xmlns="http://www.w3.org/1999/html">
+        :host {
+          display: block;
+          color: white;
+          font-family: Arial, Helvetica, sans-serif;
+        }
+        .artist-container{
+          position: relative;
+          background: linear-gradient(to right, transparent , #000000) no-repeat, url("./resources/Adele.jpg") no-repeat, black;
+          background-size: 200px;
+          height: 100px;
+          padding: 20px 20px 20px 200px;
+        }
+        .artist-alias{
+          font-weight: bold;
+          font-size: 2em;
+        }
+        .artist-genre{
+          font-weight: bold;
+          font-size: 1.24em;
+        }
+        .socialmedia-bar{
+          position: absolute;
+          right: 20px;
+          bottom: 20px;
+        }
+        .socialmedia-icon{
+          width: 20px;
+        }
+      </style>
+      <div class="artist-container">
+        <div class="artist-alias">[[artist.alias]]</div>
+        <div class="artist-genre">[[artist.genre]]</div>
+        <div class="socialmedia-bar">
+          <a href="[[artist.facebookURI]]">
+            <img class="socialmedia-icon" src="./resources/flogo-HexRBG-Wht-58.png" alt="facebook">
+          </a>
+          
+          <a href="[[artist.twitterURI]]">
+            <img class="socialmedia-icon" src="./resources/Twitter_Social_Icon_Square_White.png" alt="twitter">
+          </a>
+          <a href="[[artist.mtvURI]]">
+            <img class="socialmedia-icon" src="./resources/mtv-icon-white-200px.png" alt="mtv">
+          </a>
+        </div>
+      </div>
+    `;
+  }
+  static get properties() {
+    return {
+      artist: {
+        type: Object
+      },
+    };
+  }
+}
+
+window.customElements.define('artist-element', ArtistElement);
